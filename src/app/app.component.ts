4a842ec895a3f4ffb8baaaf1aa64fdab1ec21bf1
stnor <stefan@selessia.com>
2024-10-21 11:04:06 +0200

    Some testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,32 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lazy-deps';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+    (async () => {
+      const jQueryAwait = await import("jquery");
+      console.log("await import type=", typeof jQueryAwait)
+      console.log("await import jQueryAwait=", jQueryAwait)
+
+      //@ts-ignore
+      console.log("await import jQueryAwait.module=", jQueryAwait.module)
+
+      //@ts-ignore
+      const jQuery = require("jquery");
+      console.log("using require", typeof jQuery, jQuery);
+
+    })();
+
+
+
+  }
+
+
 }
